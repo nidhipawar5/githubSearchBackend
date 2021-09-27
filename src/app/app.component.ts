2827703db834cd5f847168fe72a504f8ec10ba6e
nidhipawar5 <nidhipawar0504@gmail.com>
2021-09-27 16:22:12 +0530

    styling added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,19 @@ import { GetApiService } from './services/get-api.service';
 export class AppComponent {
   user:any;
   repos:any;
+
+  data:Array<any>
+  totalRecords:Number
+  page:Number=1
   
   title = 'fyle-project';
   constructor(private api:GetApiService){
     console.log('Github Component Init...');
+    
   }
+  
   ngOnInit(){
-   // this.api.getProfileInfo().subscribe((data)=>{
-    //  console.warn("get api data",data);
-    //  this.title=data['title'];
-    //})
+   
   
   }
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,20 @@ import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { GetApiService } from './services/get-api.service';
 import { ProfileComponent } from './profile/profile.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent, 
+    
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ],
   providers: [GetApiService],
   bootstrap: [AppComponent]
